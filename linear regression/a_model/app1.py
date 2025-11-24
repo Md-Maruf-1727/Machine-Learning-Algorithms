78bd57a26037b22a0dfc2a8cfdd2bf3f3ed90f83
Md-Maruf-1727 <maruf.md1727@gmail.com>
2025-11-24 22:44:10 +0600

    a model added

--- linear regression/a_model/app1.py
+++ linear regression/a_model/app1.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__, template_folder="tamplate")
+model = pickle.load(open("linear regression/a_model/height.pkl", "rb"))
 
 @app.route("/")
 def home():
@@ -10,6 +12,8 @@ def home():
 def pred():
     data = float(request.form.get("height"))
     feature = [[data]]
+    prediction = model.predict(feature)
+    return render_template("index.html", Predicted_Data = f"Weight is ! {prediction}")
 
 if __name__ == ("__main__"):
     app.run(debug=True)
