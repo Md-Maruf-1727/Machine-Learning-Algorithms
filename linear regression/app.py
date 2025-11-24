034ef9ef0b85d314ab7840c3cfad7c12077f28b5
Md-Maruf-1727 <maruf.md1727@gmail.com>
2025-11-24 21:17:17 +0600

    Updated linear 4

--- linear regression/app.py
+++ linear regression/app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def wel():
+    return "Welcome in my home"
+
+@app.route("/home")
 def home():
-    return "This is my home"
+    return "This is my homeland"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
